a3a63cbf9cc5700590592122ff2f9bdabc1059c0
user <user@DESKTOP-F6RQIHQ>
2026-03-05 17:57:20 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 20F);
+            button1.Location = new Point(375, 328);
+            button1.Name = "button1";
+            button1.Size = new Size(219, 71);
+            button1.TabIndex = 0;
+            button1.Text = "배경색 바꾸기";
+            button1.UseMnemonic = false;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 30F, FontStyle.Bold);
+            label1.Location = new Point(311, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 54);
+            label1.TabIndex = 1;
+            label1.Text = "유정인";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 20F);
+            label2.Location = new Point(311, 160);
+            label2.Name = "label2";
+            label2.Size = new Size(283, 74);
+            label2.TabIndex = 2;
+            label2.Text = "수원대 컴퓨터SW학과\r\n25017056";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.화면_캡처_2026_03_05_165815;
+            pictureBox1.Location = new Point(48, 92);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(192, 254);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+    }
+}
