df3e601f08975f84ef49ec077bb63ad634ca4c56
yangnyeom06 <lyujin1204@gmail.com>
2026-03-05 23:33:53 +0900

    이미지 마우스 인식, 사진 전환, 깃허브 하이퍼링크 추가

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,64 @@
+using System.Diagnostics;
+
 namespace WinFormsApp3
 {
     public partial class Form1 : Form
     {
+
+        bool booleans = true;
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.MouseEnter += pictureBox1_MouseEnter;
+            pictureBox1.MouseLeave += pictureBox1_MouseLeave;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Random rand = new Random();
-            this.BackColor = Color.FromArgb(rand.Next(200), rand.Next(200), rand.Next(200));        
+            this.BackColor = Color.FromArgb(rand.Next(200), rand.Next(200), rand.Next(200));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (booleans)
+            {
+                pictureBox1.Image = Properties.Resources.SW융합대학_로고;
+                booleans = false;
+            }
+            else
+            {
+                pictureBox1.Image = Properties.Resources.화면_캡처_2026_03_05_165815;
+                booleans = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "https://github.com/Yangnyeom06/WinFormsAppNameCard",
+                UseShellExecute = true
+            };
+
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.BorderStyle = BorderStyle.None;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
